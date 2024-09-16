1514acb8fd03d4917532f56b7dc977ec09a2cb64
YuanaHao <youthandqueen@qq.com>
2024-09-16 18:08:37 +0800

    settings

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -73,12 +73,12 @@ export default hopeTheme({
   blog: {
     articleInfo: ["Date", "PageView", "Category", "Tag", "ReadingTime"],
     name: "YuanaHao",
-    avatar: "../../picture/figure.jpg",
+    avatar: "/figure.jpg",
     description: "零基础小白学习计算机ing",
     intro: "/intro.html",
     medias: {
       GitHub: "https://github.com/YuanaHao",
-      Wechat: "../../picture/WeChat.jpg",
+      Wechat: "/WeChat.jpg",
       Email: "mailto:2131693324@qq.com",
     },
   },
