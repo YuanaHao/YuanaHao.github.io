abdcf6f58f4772625360dbcacd5486d6ac32b687
YuanaHao <youthandqueen@qq.com>
2024-09-17 15:27:33 +0800

    update mathjax

---
+++ docs/.vuepress/components/MathJax.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="mathjax-container">
+        <div ref="mathjaxRef"></div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, watch } from "vue";
+import { initMathJax } from "@/plugins/mathjax";
+
+const mathjaxRef = ref<HTMLDivElement>();
+
+const props = defineProps({
+    latex: {
+        type: String,
+        default: ""
+    },
+    previewLatex: {
+        type: String,
+        default: ""
+    }
+});
+
+const renderMathJax = () => {
+    initMathJax({}, async () => {
+        // window.MathJax.tex2svgPromise(props.latex, { display: true }).then((math: Element) => {
+        //     if (mathjaxRef.value) {
+        //         mathjaxRef.value.innerHTML = "";
+        //         mathjaxRef.value.appendChild(math);
+        //     }
+        // });
+        try {
+            if (!props.latex) return;
+            const node = await window.MathJax.tex2svg(props.previewLatex, {});
+            const elsvg = node.firstElementChild;
+            elsvg.removeAttribute("style");
+            elsvg.removeAttribute("focusable");
+            elsvg.removeAttribute("role");
+            if (mathjaxRef.value) {
+                mathjaxRef.value.innerHTML = "";
+                mathjaxRef.value.appendChild(elsvg);
+            }
+        } catch (err) {
+            console.log("渲染内容非法");
+        }
+    });
+};
+
+renderMathJax();
+
+watch(() => props.previewLatex, () => {
+    renderMathJax();
+});
+</script>
+
+<style lang="scss" scoped>
+.mathjax-container {
+    margin: 0 auto;
+}
+</style>
